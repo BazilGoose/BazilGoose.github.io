8f91aee5e82c9b97814bd20bd14f5647a204ab73
BazilGoose <156262039+BazilGoose@users.noreply.github.com>
2024-03-27 20:56:40 -0400

    Adding A Bunch Of Stuff

---
+++ Assignment4/Part 2/gallery-start/main.js
@@ -0,0 +1,20 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Declaring the array of image filenames */
+const fileNames = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
+
+/* Declaring the alternative text for each image file */
+const altNames = ["Picture of eye", "Waves", "Flowers", "Ancient Egyption Hieroglyphs", "Butterfly"];
+
+/* Looping through images */
+for (let i = 0; i < fileNames.length; i++){
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `../${fileNames[i]}`);
+    newImage.setAttribute('alt', `${altNames[i]}`);
+    thumbBar.appendChild(newImage);
+};
+/* Wiring up the Darken/Lighten button */
